ef66e4d520ec5ce609870cca3b7052cf415659eb
2018210429 <46084069+2018210429@users.noreply.github.com>
2020-06-02 21:51:44 +0800

    Add files via upload

---
+++ 选择3.0/pages/list/list.js
@@ -0,0 +1,122 @@
+// pages/list/list.js
+Page({
+  data: {
+    content: [],
+    weather: [],
+    page: 0,
+  },
+  onLoad: function (e) {
+    var that = this;
+    var app = getApp();
+    that.setData({
+      content:[],
+      weather:[],
+    })
+    wx.request({
+      method: 'GET',
+      url: 'http://api.map.baidu.com/place/v2/search',
+      data: {
+        ak: '32cxFBvTGurCaAoG2BLUo36VEvxxZMQm',
+        query: '旅游景点',
+        region: app.globalData.points,
+        output: 'json',
+        scope: '2',
+        page_size: '20',
+        page_num: '0',
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        //console.log(res);
+        that.setData({
+          page: parseInt(res.data.total / 20),
+        })
+        for (var j = 0; j < that.data.page + 1; j++) {
+          wx.request({
+            method: 'GET',
+            url: 'http://api.map.baidu.com/place/v2/search',
+            data: {
+              ak: '32cxFBvTGurCaAoG2BLUo36VEvxxZMQm',
+              query: '旅游景点',
+              region: app.globalData.points,
+              output: 'json',
+              scope: '2',
+              page_size: '20',
+              page_num: j,
+            },
+            header: {
+              'content-type': 'application/json'
+            },
+            success: function (res) {
+              //console.log(res);
+              let content = that.data.content
+              for (var i = 0; i < res.data.results.length; i++) {
+                var obj = {};
+                obj.id = res.data.results[i].uid;
+                obj.name = res.data.results[i].name;
+                obj.location=res.data.results[i].location;
+                obj.detailurl=res.data.results[i].detail_info.detail_url;
+                content.push(obj);
+              };
+              that.setData({
+                content: content
+              });
+            },
+            fail: function (res) {
+              console.log("fail!!!")
+            },
+            complete: function (res) {
+            }
+          })
+        }
+        //console.log(that.data.content);
+      },
+      fail: function (res) {
+        console.log("fail!!!")
+      },
+      complete: function (res) {
+        console.log("end")
+      }
+    })
+    wx.request({
+      method: 'GET',
+      url: 'https://tianqiapi.com/free/week',
+      data: {
+        appid: '59458163',
+        appsecret: 'XZKEs05C',
+        city: app.globalData.points,
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        let weather = that.data.weather
+        // console.log(res);
+        for (var i = 0; i < 7; i++) {
+          var obj = {};
+          obj = res.data.data[i]
+          weather.push(obj);
+        };
+        that.setData({
+          weather:weather
+        })
+        //console.log(weather)
+      },
+      fail: function (res) {
+      },
+      complete: function (res) {
+        console.log("end")
+      }
+    })
+  },
+  detail: function (e) {
+    var app = getApp();
+    // console.log(e);
+    app.globalData.details = this.data.content[e.currentTarget.dataset.key];
+    //console.log(app.globalData.details)
+    wx.navigateTo({
+      url: '../place/place'
+    })
+  },
+})
---
+++ 选择3.0/pages/place/place.js
@@ -0,0 +1,60 @@
+// pages/place/place.js
+Page({
+  data: {
+    detail: [],
+    page: 0
+  },
+  onLoad: function (e) {
+    var that = this;
+    var app = getApp();
+    wx.request({
+      method: 'GET',
+      url: 'http://api.map.baidu.com/place/v2/detail',
+      data: {
+        ak: '32cxFBvTGurCaAoG2BLUo36VEvxxZMQm',
+        uid: app.globalData.details.id,
+        output: 'json',
+        scope: 2
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        // console.log(res);
+        let detail = that.data.detail
+        var obj = {};
+        obj.adress = res.data.result.address;
+        obj.name = res.data.result.name;
+        obj.area = res.data.result.are;
+        obj.city = res.data.result.city;
+        obj.province = res.data.result.province;
+        obj.id = res.data.result.uid;
+        obj.street_id = res.data.result.street_id;
+        obj.location = res.data.result.location;
+        obj.rating = res.data.result.detail_info.overall_rating;
+        obj.content = res.data.result.detail_info.content_tag;
+        obj.price = res.data.result.detail_info.price;
+        obj.grade = res.data.result.detail_info.scope_grade;
+        obj.type = res.data.result.detail_info.scope_type;
+        obj.hours = res.data.result.detail_info.shop_hours;
+        detail.push(obj);
+        that.setData({
+          detail:detail
+        })
+        var app = getApp();
+        app.globalData.baiduurl = res.data.result.detail_info.detail_url;
+      },
+      fail: function (res) {
+        console.log("fail!!!")
+      },
+      complete: function (res) {
+        console.log("end")
+      },
+    })
+  },
+  changetobaidu:function(){
+    wx.navigateTo({
+      url: '../map/map'
+    })
+  }
+})
